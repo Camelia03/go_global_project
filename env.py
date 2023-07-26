5b537e47aea60bbb1941170bbebf468222db5c0a
Camelia03 <camelia_weber@yahoo.com>
2023-07-26 15:31:19 +0000

    Get the static and media files stored on Clodinary and deploy to Heroku

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = 'postgres://nxhpuelm:RyCK81P5n_0VcQa2JFn1DfJhR5wBcQ7z@trumpet.db.elephantsql.com/nxhpuelm'
 os.environ["SECRET_KEY"] = "my_super^secret@key007"
+os.environ["CLOUDINARY_URL"] = 'cloudinary://469575875424773:TVcPWjO5rP_ULrqG6RI5TNGiuQE@ddoeilp3b'
