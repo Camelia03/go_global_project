aaa854afa781b7948d6ab35f972e0bd0c47749cd
Camelia03 <camelia_weber@yahoo.com>
2023-07-26 15:12:18 +0000

    Modyfing settings.py

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = 'postgres://nxhpuelm:RyCK81P5n_0VcQa2JFn1DfJhR5wBcQ7z@trumpet.db.elephantsql.com/nxhpuelm'
+os.environ["SECRET_KEY"] = "my_super^secret@key007"
